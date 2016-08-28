e2ed9a04089001bf355d2efecfe9a280d3c5426e
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-08-28 13:37:02 +0100

    Created auth service to handle authentications

---
+++ auth/app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var xmlparser = require('express-xml-bodyparser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use('/static', express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(xmlparser({trim: false, explicitArray: false}));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.sendStatus(err.status || 500);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.sendStatus(err.status || 500);
+});
+
+
+module.exports = app;
---
+++ auth/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router  = express.Router(),
+    tokens  = [];
+
+router.get('/ping', function(req, res) {
+  res.sendStatus(201);
+});
+
+router.post('/auth', function(req, res){
+  if(req.body.username == "admin" && req.body.password == "password"){
+    var token = Math.random().toString(36).substr(2);
+    tokens.push(token);
+
+    res.cookie('token', token)
+
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.post('/validate', function(req, res){
+  if(tokens.indexOf(req.body.token) > -1){
+      res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.post('/logout', function(req, res){
+  tokens.splice(tokens.indexOf(req.body.token), 1)
+
+  res.sendStatus(200);
+})
+
+module.exports = router;
