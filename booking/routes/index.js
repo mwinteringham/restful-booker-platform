7119e9aa0d18adf15d23efb4952037e634bbdb60
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-08-28 21:20:06 +0100

    Authentication in place for POST, PUT and DELETE for booking service

--- booking/routes/index.js
+++ booking/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router  = express.Router(),
     parse   = require('../helpers/parser'),
     crypto = require('crypto'),
+    request = require('request'),
     Booking = require('../models/booking'),
     Counter = require('../models/counters');
 
@@ -28,47 +29,89 @@ router.get('/booking/:id',function(req, res, next){
 router.post('/booking', function(req, res, next) {
   newBooking = req.body;
 
-  Booking.create(newBooking, function(err, booking){
-    if(err)
-      res.sendStatus(500);
-    else {
-      var record = parse.bookingWithId(req, booking);
-
-      if(!record){
-        res.sendStatus(418);
-      } else {
-        res.send(record);
-      }
+  var options = {
+    uri: 'http://localhost:3004/validate',
+    method: 'POST',
+    json: {
+      "token": req.cookies.token
     }
-  })
+  };
+
+  request(options, function(error, response, body){
+    if(response.statusCode == 200){
+      Booking.create(newBooking, function(err, booking){
+        if(err)
+          res.sendStatus(500);
+        else {
+          var record = parse.bookingWithId(req, booking);
+
+          if(!record){
+            res.sendStatus(418);
+          } else {
+            res.send(record);
+          }
+        }
+      });
+    } else {
+      res.sendStatus(403);
+    }
+  });
 });
 
 router.put('/booking/:id', function(req, res, next) {
-  Booking.update(req.params.id, req.body, function(err){
-    Booking.get(req.params.id, function(err, record){
-      if(record){
-        var booking = parse.booking(req.headers.accept, record);
+  var options = {
+    uri: 'http://localhost:3004/validate',
+    method: 'POST',
+    json: {
+      "token": req.cookies.token
+    }
+  };
 
-        if(!booking){
-          res.sendStatus(418);
-        } else {
-          res.send(booking);
-        }
-      } else {
-        res.sendStatus(405);
-      }
-    })
-  })
+  request(options, function(error, response, body){
+    if(response.statusCode == 200){
+      Booking.update(req.params.id, req.body, function(err){
+        Booking.get(req.params.id, function(err, record){
+          if(record){
+            var booking = parse.booking(req.headers.accept, record);
+
+            if(!booking){
+              res.sendStatus(418);
+            } else {
+              res.send(booking);
+            }
+          } else {
+            res.sendStatus(405);
+          }
+        })
+      });
+    } else {
+      res.sendStatus(403);
+    }
+  });
 });
 
 router.delete('/booking/:id', function(req, res, next) {
-  Booking.get(req.params.id, function(err, record){
-    if(record){
-      Booking.delete(req.params.id, function(err){
-          res.sendStatus(201);
+  var options = {
+    uri: 'http://localhost:3004/validate',
+    method: 'POST',
+    json: {
+      "token": req.cookies.token
+    }
+  };
+
+  request(options, function(error, response, body){
+    if(response.statusCode == 200){
+      Booking.get(req.params.id, function(err, record){
+        if(record){
+          Booking.delete(req.params.id, function(err){
+              res.sendStatus(201);
+          });
+        } else {
+          res.sendStatus(405);
+        }
       });
     } else {
-      res.sendStatus(405);
+      res.sendStatus(403);
     }
   });
 });
--- booking/tests/spec.js
+++ booking/tests/spec.js
@@ -58,44 +58,80 @@ describe('restful-booker-platform - POST /booking', function () {
   })
 
   it('responds with the created booking and assigned booking id', function testCreateBooking(done){
-    request(server)
-      .post('/booking')
-      .set('Accept', 'application/json')
-      .send(payload)
-      .expect(200)
-      .expect(function(res){
-        res.body.bookingid.should.equal(1);
-        res.body.booking.should.deep.equal(payload);
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
       })
-      .end(done)
+      .then(function(res){
+        request(server)
+          .post('/booking')
+          .set('Accept', 'application/json')
+          .set('Cookie', 'token=' + res.body.token)
+          .send(payload)
+          .expect(200)
+          .expect(function(res){
+            res.body.bookingid.should.equal(1);
+            res.body.booking.should.deep.equal(payload);
+          })
+          .end(done)
+      })
   });
 
   it('responds with a 500 error when a bad payload is sent', function testCreateBadBooking(done){
     badpayload = { 'lastname': 'Brown', 'totalprice': 111, 'depositpaid': true, 'additionalneeds': 'Breakfast'}
 
-    request(server)
-      .post('/booking')
-      .send(badpayload)
-      .expect(500, done);
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
+      .then(function(res){
+        request(server)
+          .post('/booking')
+          .set('Cookie', 'token=' + res.body.token)
+          .send(badpayload)
+          .expect(500, done);
+      });
   });
 
   it('responds with a 200 when a payload with too many params are sent', function testCreateExtraPayload(done){
     var extraPayload = payload
     extraPayload.extra = 'bad'
 
-    request(server)
-      .post('/booking')
-      .set('Accept', 'application/json')
-      .send(extraPayload)
-      .expect(200, done);
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
+      .then(function(res){
+        request(server)
+          .post('/booking')
+          .set('Accept', 'application/json')
+          .set('Cookie', 'token=' + res.body.token)
+          .send(extraPayload)
+          .expect(200, done);
+      })
   });
 
   it('responds with a 418 when using a bad accept header', function testTeapot(done){
-    request(server)
-      .post('/booking')
-      .set('Accept', 'application/ogg')
-      .send(payload)
-      .expect(418, done)
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
+      .then(function(res){
+        request(server)
+          .post('/booking')
+          .set('Accept', 'application/ogg')
+          .set('Cookie', 'token=' + res.body.token)
+          .send(payload)
+          .expect(418, done)
+      });
   })
 });
 
@@ -105,20 +141,34 @@ describe('restful-booker-platform POST /booking - multiple bookings', function()
   })
 
   it('responds with the correct assigned booking id when multiple payloads are sent', function testBookingId(done){
-    request(server)
-      .post('/booking')
-      .send(payload)
+    var token;
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
+      .then(function(res){
+        return token = res.body.token
+      })
+      .then(function(){
+        return request(server)
+          .post('/booking')
+          .set('Cookie', 'token=' + token)
+          .send(payload)
+      })
       .then(function(){
         request(server)
           .post('/booking')
           .send(payload2)
           .set('Accept', 'application/json')
+          .set('Cookie', 'token=' + token)
           .expect(200)
           .expect(function(res) {
             res.body.bookingid.should.equal(2);
           })
           .end(done)
-      })
+      });
   });
 
 });
@@ -126,40 +176,80 @@ describe('restful-booker-platform POST /booking - multiple bookings', function()
 describe('restful-booker-platform - PUT /booking', function () {
 
   it('responds with a 200 and an updated payload', function testUpdatingABooking(done){
-    request(server)
-      .post('/booking')
-      .send(payload)
+    var token;
+
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
       .then(function(res){
+        return token = res.body.token;
+      })
+      .then(function(){
+        return request(server)
+          .post('/booking')
+          .set('Cookie', 'token=' + token)
+          .send(payload)
+      })
+      .then(function(){
         request(server)
           .put('/booking/1')
           .set('Accept', 'application/json')
+          .set('Cookie', 'token=' + token)
           .send(payload2)
           .expect(200)
-          .expect(payload2, done);
+          .expect(payload2)
+          .end(done);
       });
   });
 
   it('responds with a 200 and an updated payload using auth', function testUpdatingABooking(done){
-    request(server)
-      .post('/booking')
-      .send(payload)
+    var token;
+
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
       .then(function(res){
+        return token = res.body.token;
+      })
+      .then(function(){
+        return request(server)
+          .post('/booking')
+          .set('Cookie', 'token=' + token)
+          .send(payload)
+      })
+      .then(function(){
         request(server)
           .put('/booking/1')
           .set('Accept', 'application/json')
-          .set('Authorization', 'Basic YWRtaW46cGFzc3dvcmQxMjM=')
+          .set('Cookie', 'token=' + token)
           .send(payload2)
           .expect(200)
-          .expect(payload2, done);
-      });
+          .expect(payload2)
+          .end(done)
+      })
   });
 
   it('responds with a 405 when attempting to update a booking that does not exist', function testUpdatingNonExistantBooking(done){
-    request(server)
-      .put('/booking/100000')
-      .set('Accept', 'application/json')
-      .send(payload2)
-      .expect(405, done);
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
+      .then(function(res){
+        request(server)
+          .put('/booking/100000')
+          .set('Accept', 'application/json')
+          .set('Cookie', 'token=' + res.body.token)
+          .send(payload2)
+          .expect(405, done);
+      })
   });
 
 });
@@ -167,40 +257,81 @@ describe('restful-booker-platform - PUT /booking', function () {
 describe('restful-booker-platform DELETE /booking', function(){
 
   it('responds with a 201 when deleting an existing booking', function testDeletingAValidBooking(done){
-    request(server)
-      .post('/booking')
-      .send(payload)
+    var token;
+
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
       .then(function(res){
+        return token = res.body.token;
+      })
+      .then(function(){
+        return request(server)
+          .post('/booking')
+          .set('Cookie', 'token=' + token)
+          .send(payload)
+      })
+      .then(function(){
         return request(server)
           .delete('/booking/1')
+          .set('Cookie', 'token=' + token)
           .expect(201)
       }).then(function(){
         request(server)
           .get('/booking/1')
-          .expect(404, done)
+          .expect(404)
+          .end(done)
       });
   });
 
   it('responds with a 201 when deleting an existing booking with a basic auth header', function testDeletingAValidBookingWithAuth(done){
-    request(server)
-      .post('/booking')
-      .send(payload)
+    var token;
+
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
       .then(function(res){
+        return token = res.body.token;
+      })
+      .then(function(){
+        return request(server)
+          .post('/booking')
+          .set('Cookie', 'token=' + token)
+          .send(payload)
+      })
+      .then(function(){
         return request(server)
           .delete('/booking/2')
-          .set('Authorization', 'Basic YWRtaW46cGFzc3dvcmQxMjM=')
+          .set('Cookie', 'token=' + token)
           .expect(201)
-      }).then(function(){
+      })
+      .then(function(){
         request(server)
           .get('/booking/2')
-          .expect(404, done)
+          .expect(404)
+          .end(done)
       });
   });
 
   it('responds with a 405 when deleting a non existing booking', function testDeletingNonExistantBooking(done){
-    request(server)
-      .delete('/booking/1')
-      .expect(405, done)
+    request('http://localhost:3004')
+      .post('/auth')
+      .send({
+        'username' : 'admin',
+        'password' : 'password'
+      })
+      .then(function(res){
+        request(server)
+          .delete('/booking/1')
+          .set('Cookie', 'token=' + res.body.token)
+          .expect(405, done)
+      })
   });
 
 });
