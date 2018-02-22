0890830f1028a371b742316d1beb74c7b04a312b
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-02-22 11:52:37 +0000

    Ability to create hotels added

--- new-ui/src/js/components/Hotel.jsx
+++ new-ui/src/js/components/Hotel.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Hotel extends React.Component {
 
     constructor() {
         super();
 
-        this.navigateToHotel = this.navigateToHotel.bind(this);
         this.deleteBooking = this.deleteBooking.bind(this);
-    }
-
-    navigateToHotel(){        
-        // const { history } = this.props;
-        // history.push('/hotel/' + this.props.details.hotelid);
     }
 
     deleteBooking(){
@@ -36,21 +30,15 @@ export default class Hotel extends React.Component {
 
         return(
             <div className="row" onClick={() => this.navigateToHotel()}>
-                <div className="col-sm-2"><p>{this.props.details.name}</p></div>
-                <div className="col-sm-3"><p>{this.props.details.address}</p></div>
-                <div className="col-sm-2"><p>{this.props.details.contact.name}</p></div>
-                <div className="col-sm-2"><p>{this.props.details.contact.phone}</p></div>
-                <div className="col-sm-2"><p>{this.props.details.contact.email}</p></div>
+                <Link to={"/hotel/" + this.props.details.hotelid}>
+                    <div className="col-sm-2"><p>{this.props.details.name}</p></div>
+                    <div className="col-sm-3"><p>{this.props.details.address}</p></div>
+                    <div className="col-sm-2"><p>{this.props.details.contact.name}</p></div>
+                    <div className="col-sm-2"><p>{this.props.details.contact.phone}</p></div>
+                    <div className="col-sm-2"><p>{this.props.details.contact.email}</p></div>
+                </Link>
                 <div className="col-sm-1">{deleteHotel}</div>
             </div>
         );
     }
 }
-
-// Hotel.propTypes = {
-//     match: PropTypes.object.isRequired,
-//     location: PropTypes.object.isRequired,
-//     history: PropTypes.object.isRequired
-// }
-
-// export default withRouter(Hotel);
--- new-ui/src/js/components/Hotels.jsx
+++ new-ui/src/js/components/Hotels.jsx
@@ -6,9 +6,22 @@ export default class Hotels extends React.Component {
 
     constructor() {
         super();
-		this.state = { hotels : []};
+		this.state = {
+						hotels : [], 
+						newHotel : {
+							"name": "",
+							"address": "",
+							"regdate": "",
+							"contact": {
+								"name": "",
+								"phone": "",
+								"email": ""
+							}
+						} 
+					};
 		
 		this.updateHotels = this.updateHotels.bind(this);
+		this.createHotel = this.createHotel.bind(this);
     }
     
     componentDidMount() {
@@ -20,7 +33,6 @@ export default class Hotels extends React.Component {
 	}
 
 	updateHotels() {
-		console.log("HOTELZ");
 		fetch('http://localhost:3001/hotel')
 			.then(res => res.json())
 			.then(body => {
@@ -28,32 +40,58 @@ export default class Hotels extends React.Component {
 			});
 	}
 
+	createHotel() {
+		this.state.newHotel.regdate = new Date();
+		
+		fetch('http://localhost:3001/hotel', {
+			method: 'POST',
+			headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+			},
+			credentials: 'include',
+			body : JSON.stringify(this.state.newHotel)
+		})
+		.then(res => {
+			if(res.status == 200){
+				document.getElementById("hotelName").value = '';
+				document.getElementById("address").value = '';
+				document.getElementById("owner").value = '';
+				document.getElementById("phone").value = '';
+				document.getElementById("email").value = '';
+				
+				this.setState({newHotel : {}})
+				this.updateHotels();
+			}
+		})
+	}
+
     render() {
       let inputFields = null;
       if(this.props.isAuthenticated){
         inputFields = <div className="row">
-						<div className="col-sm-2"><input type="text" id="hotelName" /></div>
-						<div className="col-sm-3"><input type="text" id="address" /></div>
-						<div className="col-sm-2"><input type="text" id="owner" /></div>
-						<div className="col-sm-2"><input type="text" id="phone" /></div>
-						<div className="col-sm-2"><input type="text" id="email" /></div>
-						<div className="col-sm-1"><input type="button" value="Create" id="createHotel"/></div>
+						<div className="col-sm-2"><input type="text" id="hotelName" onChange={val => this.state.newHotel.name = val.target.value} /></div>
+						<div className="col-sm-3"><input type="text" id="address" onChange={val => this.state.newHotel.address = val.target.value} /></div>
+						<div className="col-sm-2"><input type="text" id="owner" onChange={val => this.state.newHotel.contact.name = val.target.value} /></div>
+						<div className="col-sm-2"><input type="text" id="phone" onChange={val => this.state.newHotel.contact.phone = val.target.value} /></div>
+						<div className="col-sm-2"><input type="text" id="email" onChange={val => this.state.newHotel.contact.email = val.target.value} /></div>
+						<div className="col-sm-1"><input type="button" value="Create" id="createHotel" onClick={this.createHotel}/></div>
                       </div>
       }
       return(
         <div>
-          <div className="row">
-            <div className="col-sm-2 rowHeader"><p>Hotel name</p></div>
-            <div className="col-sm-3 rowHeader"><p>Address</p></div>
-            <div className="col-sm-2 rowHeader"><p>Owner</p></div>
-            <div className="col-sm-2 rowHeader"><p>Phone number</p></div>
-            <div className="col-sm-2 rowHeader"><p>Email</p></div>
-            <div className="col-sm-1"></div>
-          </div>
-          {this.state.hotels.map((hotel, index) => {
-            return <div key={hotel.hotelid}><Hotel details={hotel} updateHotels={this.updateHotels} isAuthenticated={this.props.isAuthenticated}/></div>
-		  })}
-		  {inputFields}
+			<div className="row">
+				<div className="col-sm-2 rowHeader"><p>Hotel name</p></div>
+				<div className="col-sm-3 rowHeader"><p>Address</p></div>
+				<div className="col-sm-2 rowHeader"><p>Owner</p></div>
+				<div className="col-sm-2 rowHeader"><p>Phone number</p></div>
+				<div className="col-sm-2 rowHeader"><p>Email</p></div>
+				<div className="col-sm-1"></div>
+			</div>
+			{this.state.hotels.map((hotel, index) => {
+				return <div key={hotel.hotelid}><Hotel details={hotel} updateHotels={this.updateHotels} isAuthenticated={this.props.isAuthenticated}/></div>
+			})}
+			{inputFields}
         </div>
       );
     }
