ea81b890216215e1b41a9632b4e94b0aa946497e
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-03-11 13:32:09 +0000

    Database seeding and folder updates

--- db/setup.sql
+++ db/setup.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE rbp;
 USE rbp;
-CREATE table BOOKINGS ( bookingid int NOT NULL AUTO_INCREMENT, hotelid int, firstname varchar(255), lastname varchar(255), totalprice int, depositpaid Boolean, checkin Date, checkout Date, primary key (bookingid));
 CREATE table HOTELS ( hotelid int NOT NULL AUTO_INCREMENT, name varchar(255), address varchar(255), regdate Date, contactName varchar(255), contactPhone varchar(255), contactEmail varchar(255), primary key (hotelid));
+CREATE table BOOKINGS ( bookingid int NOT NULL AUTO_INCREMENT, hotelid int, firstname varchar(255), lastname varchar(255), totalprice int, depositpaid Boolean, checkin Date, checkout Date, primary key (bookingid));
+
+INSERT INTO HOTELS (name, address, regdate, contactName, contactPhone, contactEmail) VALUES('Hilton', '52 The Street, City', '2018-01-01', 'Mark', '01612829348', 'mark@hilton.com');
+INSERT INTO BOOKINGS(hotelid, firstname, lastname, totalprice, depositpaid, checkin, checkout) VALUES(1,'James','Dean',100,true,'2018-02-26','2018-02-26');
