9ab357aeb63a155f20fe480124e75e654454399f
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-06-06 21:14:08 +0100

    Updated to allow rbp to run locally on Windows

--- run_local.cmd
+++ run_local.cmd
@@ -1,9 +1,9 @@
-mvn clean install
+call mvn clean install
 
-java -jar auth/target/restful-booker-platform-auth-*-SNAPSHOT.jar &
-java -jar booking/target/restful-booker-platform-booking-*-SNAPSHOT.jar &
-java -jar hotel/target/restful-booker-platform-hotel-*-SNAPSHOT.jar &
-java -jar report/target/restful-booker-platform-report-*-SNAPSHOT.jar &
-java -jar search/target/restful-booker-platform-search-*-SNAPSHOT.jar &
+START java -jar auth/target/restful-booker-platform-auth-*-SNAPSHOT.jar
+START java -jar booking/target/restful-booker-platform-booking-*-SNAPSHOT.jar
+START java -jar hotel/target/restful-booker-platform-hotel-*-SNAPSHOT.jar
+START java -jar report/target/restful-booker-platform-report-*-SNAPSHOT.jar
+START java -jar search/target/restful-booker-platform-search-*-SNAPSHOT.jar
 
 cd ui && npm start
