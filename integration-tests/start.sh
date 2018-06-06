b6372381468d2c4495065b8ff19f1583a45cf3d1
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-06-06 08:09:43 +0100

    Updating integration tests to pass in CircleCI

---
+++ integration-tests/start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+java -jar ../booking/target/restful-booker-platform-booking-1.0-SNAPSHOT.jar &
+java -jar ../report/target/restful-booker-platform-report-1.0-SNAPSHOT.jar &
+java -jar ../auth/target/restful-booker-platform-auth-1.0-SNAPSHOT.jar &
+
+sleep 5
+
+mvn clean integration-test
