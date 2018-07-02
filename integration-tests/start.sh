beb78c5680da2664995398a0fa1388a66446b9f0
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-07-02 20:24:04 +0100

    Updating integration test timeout

--- integration-tests/start.sh
+++ integration-tests/start.sh
@@ -4,6 +4,6 @@ java -jar ../booking/target/restful-booker-platform-booking-1.0-SNAPSHOT.jar &
 java -jar ../report/target/restful-booker-platform-report-1.0-SNAPSHOT.jar &
 java -jar ../auth/target/restful-booker-platform-auth-1.0-SNAPSHOT.jar &
 
-sleep 5
+sleep 10
 
 mvn clean integration-test
