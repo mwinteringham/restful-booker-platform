dac885bb3d1d3bbe682ab81429d74c3283659a4d
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-07-24 17:56:21 +0100

    Fixed broken paths

--- ui/src/js/components/RoomListings.jsx
+++ ui/src/js/components/RoomListings.jsx
@@ -20,7 +20,7 @@ export default class RoomListings extends React.Component {
 	}
 
 	updateRooms() {
-		fetch(API_ROOT.auth + '/room')
+		fetch(API_ROOT.room + '/room')
 			.then(res => res.json())
 			.then(body => {
 				this.setState({rooms : body.rooms});
