3e66b8549d90f6a0971ea8e37d923d141a933453
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-09-05 02:45:02 +0200

    Staging another commit for lookup

--- auth/src/main/java/api/AuthApplication.java
+++ auth/src/main/java/api/AuthApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan()
 public class AuthApplication {
 
+    // Testing the lookup by making a change in search
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
