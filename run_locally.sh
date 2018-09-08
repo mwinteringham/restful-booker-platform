0f987225d7b2ad5e4f06089ef1780502386c684b
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-09-08 19:50:29 +0100

    Updated run_locally.sh to handle new UI and pre-reqs [ci skip]

--- run_locally.sh
+++ run_locally.sh
@@ -1,30 +1,78 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo ######### PRE FLIGHT CHECKS BEFORE BUILD #########
+printf "####### RESTFUL-BOOKER-PLATFORM #######
+####                               ####
+####       PRE FLIGHT CHECKS       ####
+####                               ####
+#######################################\n"
+
+
+if ! type -p java; then
+    echo "Java Runtime not found in path."
+    exit
+fi
+
+if ! type -p javac; then
+    echo "Java Runtime not found in path."
+    exit
+fi
+
+if ! type -p mvn; then
+    echo "Maven not found in path."
+    exit
+fi
+
+if ! type -p node; then
+    echo "Node not found in path."
+    exit
+fi
+
+if ! type -p npm; then
+    echo "NPM not found in path."
+    exit
+fi
 
 mvn clean
 
-echo ######### BUILDING SINGLE PAGE FRONTEND APP #########
+printf "\n####### RESTFUL-BOOKER-PLATFORM #######
+####                               ####
+####      BUILDING FRONTEND        ####
+####                               ####
+#######################################\n"
 
 cd ui/js
 npm install
 npm run build
 
-echo ######### BUILDING API BACKEND #########
+printf "\n####### RESTFUL-BOOKER-PLATFORM #######
+####                               ####
+####       BUILDING BACKEND        ####
+####                               ####
+#######################################\n"
 
 cd ../..
 
 mvn install
 
-echo ######### STARTING RESTFUL-BOOKER-PLATFORM #########
+printf "\n####### RESTFUL-BOOKER-PLATFORM #######
+####                               ####
+####    STARTING APPLICATION...    ####
+####                               ####\n"
 
 trap "kill 0" EXIT
 
-java -jar -Dspring.profiles.active=dev auth/target/restful-booker-platform-auth-*-SNAPSHOT.jar &
-java -jar -Dspring.profiles.active=dev booking/target/restful-booker-platform-booking-*-SNAPSHOT.jar &
-java -jar -Dspring.profiles.active=dev room/target/restful-booker-platform-room-*-SNAPSHOT.jar &
-java -jar -Dspring.profiles.active=dev report/target/restful-booker-platform-report-*-SNAPSHOT.jar &
-java -jar -Dspring.profiles.active=dev search/target/restful-booker-platform-search-*-SNAPSHOT.jar &
-java -jar -Dspring.profiles.active=dev ui/api/target/restful-booker-platform-ui-*-SNAPSHOT.jar &
+java -jar -Dspring.profiles.active=dev auth/target/restful-booker-platform-auth-*-SNAPSHOT.jar > auth.log &
+java -jar -Dspring.profiles.active=dev booking/target/restful-booker-platform-booking-*-SNAPSHOT.jar > booking.log &
+java -jar -Dspring.profiles.active=dev room/target/restful-booker-platform-room-*-SNAPSHOT.jar > room.log &
+java -jar -Dspring.profiles.active=dev report/target/restful-booker-platform-report-*-SNAPSHOT.jar > report.log &
+java -jar -Dspring.profiles.active=dev search/target/restful-booker-platform-search-*-SNAPSHOT.jar > search.log &
+java -jar -Dspring.profiles.active=dev ui/api/target/restful-booker-platform-ui-*-SNAPSHOT.jar > ui.log &
+
+printf "####      APPLICATION STARTED      ####
+####                               ####
+####         Available at:         ####
+####     http://localhost:3003     ####
+####                               ####
+#######################################"
 
 wait
