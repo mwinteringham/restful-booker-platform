206717a5421ab2ac60287ad6ad3a05bc44011723
Richard Bradshaw <richardbradshaw@gmail.com>
2018-09-23 20:53:34 +0100

    updated WebD Chromedriver

--- end-to-end-tests/src/main/java/driverfactory/DriverFactory.java
+++ end-to-end-tests/src/main/java/driverfactory/DriverFactory.java
@@ -16,12 +16,12 @@ public class DriverFactory
 
         if(OS.contains("win"))
         {
-            String pathToChromeDriver = System.getProperty("user.dir") + "/chromedrivers/windows/chromedriver.exe";
+            String pathToChromeDriver = System.getProperty("user.dir") + "/end-to-end-tests/chromedrivers/windows/chromedriver.exe";
             System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
         }
         else if (OS.contains("mac"))
         {
-            String pathToChromeDriver = System.getProperty("user.dir") + "/chromedrivers/mac/chromedriver";
+            String pathToChromeDriver = System.getProperty("user.dir") + "/end-to-end-tests/chromedrivers/mac/chromedriver";
             System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
         }
 
