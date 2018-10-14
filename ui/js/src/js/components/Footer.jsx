ce41e701f8713a2716026bcdf9c7a3b7b70d1325
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-10-14 12:41:23 +0100

    Adding in cookie policy

--- ui/js/src/js/components/Footer.jsx
+++ ui/js/src/js/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default class Footer extends React.Component {
 
@@ -7,9 +8,10 @@ export default class Footer extends React.Component {
     }
 
     render() {
-      return(<footer className="footer">
+      return(
+            <footer id="footer" className="footer">
                 <div className="container">
-                    <p className="text-muted">restful-booker-platform v3.0 - Created by <a href="http://www.mwtestconsultancy.co.uk">Mark Winteringham</a> - &copy; 2018 <span style={{float : "right"}}>Learn more about <a href="https://automationintesting.com" alt="Automation in testing site">Automation in Testing</a> here</span></p>
+                    <p className="text-muted">restful-booker-platform v3.0 - Created by <a href="http://www.mwtestconsultancy.co.uk">Mark Winteringham</a> - &copy; 2018 <Link to={'/cookiepolicy'}>Cookie-Policy</Link> <span style={{float : "right"}}>Learn more about <a href="https://automationintesting.com" alt="Automation in testing site">Automation in Testing</a> here</span></p>
                 </div>
             </footer>);
     }
