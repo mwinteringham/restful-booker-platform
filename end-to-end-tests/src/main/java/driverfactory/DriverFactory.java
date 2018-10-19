5d63762d7d790ca4daa14f50b95da99310ef7483
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-10-19 11:35:23 +0100

    Added in ability to configure e2e to connect to Sauce labs with correct credentials [ci skip]

--- end-to-end-tests/src/main/java/driverfactory/DriverFactory.java
+++ end-to-end-tests/src/main/java/driverfactory/DriverFactory.java
@@ -2,30 +2,64 @@ package driverfactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory
 {
     private static String OS = System.getProperty("os.name").toLowerCase();
 
-    public WebDriver create()
-    {
-        return generateDriver();
+    public WebDriver create() {
+        if(System.getenv("browser") != null){
+            if(System.getenv("browser").equals("chrome")){
+                return prepareChromeDriver();
+            } else if (System.getenv("browser").equals("remote")){
+                return prepareRemoteDriver();
+            } else {
+                System.out.println("WARN: Browser option '" + System.getenv("browser") + "' not recognised. Falling back to ChromeDriver");
+                return prepareChromeDriver();
+            }
+        }
+
+        System.out.println("WARN: No browser option detected. Defaulting to ChromeDriver but if you want to use a different browser please assign a browser to the env var 'browser'.");
+        return prepareChromeDriver();
     }
 
-    private WebDriver generateDriver(){
+    private WebDriver prepareChromeDriver(){
         String classLocation = DriverFactory.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         String pathToChromeDriver = "";
 
         if(OS.contains("win")) {
-//            pathToChromeDriver = System.getProperty("user.dir") + "/end-to-end-tests/chromedrivers/windows/chromedriver.exe";
             pathToChromeDriver = classLocation + "../..//chromedrivers/windows/chromedriver.exe";
         }
         else if (OS.contains("mac")) {
-//            pathToChromeDriver = System.getProperty("user.dir") + "/end-to-end-tests/chromedrivers/mac/chromedriver";
             pathToChromeDriver = classLocation + "../..//chromedrivers/mac/chromedriver";
         }
 
         System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
         return new ChromeDriver();
+    }
+
+    private WebDriver prepareRemoteDriver(){
+        if(System.getenv("sauce_username") == null){
+            throw new RuntimeException("To use remote driver a Sauce lab account is required. Please assign your Sauce labs account name to the environmental variable 'sauce_username'");
+        }
+
+        if(System.getenv("sauce_access_key") == null){
+            throw new RuntimeException("To use remote driver a Sauce lab account is required. Please assign your Sauce labs access key to the environmental variable 'sauce_access_key'");
+        }
+
+        String URL = "http://" + System.getenv("sauce_username") + ":" + System.getenv("sauce_access_key") + "@ondemand.saucelabs.com:80/wd/hub";
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        try {
+            return new RemoteWebDriver(new URL(URL), chromeOptions);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("WARN: An error occurred attempting to create a remote driver connection. See the following error: " + e);
+        }
     }
 }
