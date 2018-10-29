8eece31da69108c5ed28fa694c9c1d9e65b5b8c2
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-10-29 17:15:25 +0000

    Fixing broken timezone issue

--- report/src/main/java/api/ReportApplication.java
+++ report/src/main/java/api/ReportApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @ComponentScan()
 public class ReportApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReportApplication.class, args);
