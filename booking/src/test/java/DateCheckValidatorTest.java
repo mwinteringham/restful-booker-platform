d1d7b6e8cdedbd611e37849deae1247b8596dc1a
Andrew Morton <chefsim@gmail.com>
2019-01-18 08:40:28 +0000

    Validate Booking Dates are supplied and in correct order
    Fixes #48
    PR #66

---
+++ booking/src/test/java/DateCheckValidatorTest.java
@@ -0,0 +1,57 @@
+import model.BookingDates;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DateCheckValidatorTest {
+
+    private validators.DateCheckValidator dateCheckValidator;
+    private BookingDates bookingDates;
+
+    @Before
+    public void setup() {
+        dateCheckValidator = new validators.DateCheckValidator();
+        bookingDates = new BookingDates();
+    }
+
+    @Test
+    public void invalidIfDatesNotProvided() {
+        boolean validDate = dateCheckValidator.isValid(bookingDates);
+
+        assertThat("Should be invalid if dates not provided", !validDate);
+    }
+
+    @Test
+    public void invalidIfCheckinAfterCheckout() {
+        bookingDates.setCheckout(Date.valueOf("2018-01-01"));
+        bookingDates.setCheckin(Date.valueOf("2018-01-05"));
+
+        boolean validDate = dateCheckValidator.isValid(bookingDates);
+
+        assertThat("Should be invalid if checkin after checkout", !validDate);
+    }
+
+    @Test
+    public void invalidIfCheckinAndCheckoutSameDay() {
+        bookingDates.setCheckout(Date.valueOf("2018-01-01"));
+        bookingDates.setCheckin(Date.valueOf("2018-01-01"));
+
+        boolean validDate = dateCheckValidator.isValid(bookingDates);
+
+        assertThat("Should be invalid if checkin same date as checkout", !validDate);
+    }
+
+    @Test
+    public void validIfCheckinBeforeCheckout() {
+        bookingDates.setCheckin(Date.valueOf("2018-01-01"));
+        bookingDates.setCheckout(Date.valueOf("2018-01-05"));
+
+        boolean validDate = dateCheckValidator.isValid(bookingDates);
+
+        assertThat("Should be valid if checkin before checkout", validDate);
+    }
+
+}
