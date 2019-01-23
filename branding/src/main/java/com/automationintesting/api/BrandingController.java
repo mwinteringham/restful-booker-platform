879074705d21a8ee9ed47ef4a65643e634584490
Andrew Morton <chefsim@gmail.com>
2019-01-23 18:34:18 +0000

    Remove misplaced double quotes from description
    The double quotes are preventing the response from being parsed as JSON.

--- branding/src/main/java/com/automationintesting/api/BrandingController.java
+++ branding/src/main/java/com/automationintesting/api/BrandingController.java
@@ -45,7 +45,7 @@ public class BrandingController {
                 "    \"logo\": {\n" +
                 "        \"url\": \"https://www.mwtestconsultancy.co.uk/img/rbp-logo.png\"\n" +
                 "    },\n" +
-                "    \"description\": \"Welcome to Shady Meadows, a delightful Bed & Breakfast nestled in the hills on Newingtonfordburyshire. A place so beautiful you will never want to leave. All our rooms have comfortable beds and we provide breakfast from the locally sourced supermarket. It\"s a delightful place.\",\n" +
+                "    \"description\": \"Welcome to Shady Meadows, a delightful Bed & Breakfast nestled in the hills on Newingtonfordburyshire. A place so beautiful you will never want to leave. All our rooms have comfortable beds and we provide breakfast from the locally sourced supermarket. It is a delightful place.\",\n" +
                 "    \"contact\": {\n" +
                 "        \"name\": \"Shady Meadows B&B\",\n" +
                 "        \"address\": \"The Old Farmhouse, Shady Street, Newfordburyshire, NE1 410S\",\n" +
