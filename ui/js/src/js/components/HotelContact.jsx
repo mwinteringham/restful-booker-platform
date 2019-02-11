67fd9ed2730cfddb91e7aae4e7f8a0ef17ff5889
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2019-02-11 17:30:12 +0000

    Submit faked requests in the home page contact form

--- ui/js/src/js/components/HotelContact.jsx
+++ ui/js/src/js/components/HotelContact.jsx
@@ -1,46 +1,164 @@
 import React from 'react';
+import validate from 'validate.js';
+
+let rules = {
+    "name": {
+        presence : true,
+        length: {
+            minimum: 3,
+            message: "must be at least 3 characters long"
+        }
+    },
+    "email": {
+        presence : true,
+        email : true
+    },
+    "phone": {
+        presence : true,
+        numericality: {
+            onlyInteger: true
+        },
+        length: {
+            minimum: 11,
+            message: "must be at least 11 digits long"
+        }
+    },
+    "subject": {
+        presence : true,
+        length: {
+            minimum: 20,
+            message: "must be at least 20 characters long"
+        }
+    },
+    "message" : {
+        presence : true,
+        length: {
+            minimum: 20,
+            message: "must be at least 20 characters long"
+        }
+    }
+}
 
 export default class HotelContact extends React.Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            name : "",
+            email : "",
+            phone : "",
+            subject : "",
+            message : "",
+            submitted : false,
+            errors : {
+                name : [],
+                email : [],
+                phone : [],
+                subject : [],
+                message : [],
+            }
+        }
+
+        this.updateState = this.updateState.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+    }
+
+    updateState(event){
+        let currentState = this.state;
+
+        currentState[event.target.id] = event.target.value;
+
+        this.setState(currentState);
+    }
+
+    submitForm(){
+        let vErrors = validate(this.state, rules);
+
+        if(typeof vErrors === 'undefined'){
+            this.setState({ submitted : true});
+        } else {
+            this.setState({ errors : vErrors })
+        }
+    }
+
     render(){
+        let form = "";
+
+        if(this.state.submitted){
+            form = <div style={{height : "412px"}}>
+                    <h2>Thanks for getting in touch {this.state.name}!</h2>
+                    <p>We'll get back to you about</p>
+                    <p style={{fontWeight : "bold"}}>{this.state.subject}</p>
+                    <p>as soon as possible.</p>
+                    </div>
+        } else {
+            form = <form>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="basic-addon1"><span className="fa fa-id-card"></span></span>
+                        </div>
+                        <input type="text" className="form-control" placeholder="Name" aria-label="Name" id="name" aria-describedby="basic-addon1" onChange={this.updateState} />
+                        {this.state.errors.name.map((value, index) => {
+                            return <div key={"name" + index} class="invalid-feedback">
+                                {value}
+                            </div>
+                        })}
+                    </div>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="basic-addon1"><span className="fa fa-envelope"></span></span>
+                        </div>
+                        <input type="text" className="form-control" placeholder="Email" aria-label="Email" id="email" aria-describedby="basic-addon1" onChange={this.updateState} />
+                        {this.state.errors.email.map((value, index) => {
+                            return <div key={"email" + index} class="invalid-feedback">
+                                {value}
+                            </div>
+                        })}
+                    </div>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="basic-addon1"><span className="fa fa-phone"></span></span>
+                        </div>
+                        <input type="text" className="form-control" placeholder="Phone" aria-label="Phone" id="phone" aria-describedby="basic-addon1" onChange={this.updateState} />
+                        {this.state.errors.phone.map((value, index) => {
+                            return <div key={"phone" + index} class="invalid-feedback">
+                                {value}
+                            </div>
+                        })}
+                    </div>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="basic-addon1"><span className="fa fa-envelope"></span></span>
+                        </div>
+                        <input type="text" className="form-control" placeholder="Subject" aria-label="Subject" id="subject" aria-describedby="basic-addon1" onChange={this.updateState} />
+                        {this.state.errors.subject.map((value, index) => {
+                            return <div key={"subject" + index} class="invalid-feedback">
+                                {value}
+                            </div>
+                        })}
+                    </div>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">Message</span>
+                        </div>
+                        <textarea className="form-control" aria-label="Message" id="message" rows="5" onChange={this.updateState}></textarea>
+                        {this.state.errors.message.map((value, index) => {
+                            return <div key={"message" + index} class="invalid-feedback">
+                                {value}
+                            </div>
+                        })}
+                    </div>
+                    <br />
+                    <button type='button' className='btn btn-outline-secondary float-right' onClick={this.submitForm}>Submit</button>
+                </form>
+        }
+
         if(this.props.contact){
             return <div className='row contact'>
                         <div className='col-sm-1'></div>
                         <div className='col-sm-5'>
-                            <form>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="basic-addon1"><span className="fa fa-id-card"></span></span>
-                                    </div>
-                                    <input type="text" className="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" />
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="basic-addon1"><span className="fa fa-envelope"></span></span>
-                                    </div>
-                                    <input type="text" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" />
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="basic-addon1"><span className="fa fa-phone"></span></span>
-                                    </div>
-                                    <input type="text" className="form-control" placeholder="Phone" aria-label="Phone" aria-describedby="basic-addon1" />
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="basic-addon1"><span className="fa fa-envelope"></span></span>
-                                    </div>
-                                    <input type="text" className="form-control" placeholder="Subject" aria-label="Subject" aria-describedby="basic-addon1" />
-                                </div>
-                                <div className="input-group">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text">Message</span>
-                                    </div>
-                                    <textarea className="form-control" aria-label="Message" rows="5"></textarea>
-                                </div>
-                                <br />
-                                <button type='button' className='btn btn-outline-secondary float-right'>Submit</button>
-                            </form>
+                            {form}
                         </div>
                         <div className='col-sm-5'>
                             <p><span className="fa fa-home"></span> {this.props.contact.name}</p>
