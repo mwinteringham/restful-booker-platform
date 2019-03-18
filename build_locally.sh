156f143a648a423c2f0533a99e46444ea1de885c
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2019-03-18 20:47:56 +0000

    Use bash scripts to run or build project seperately

---
+++ build_locally.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+printf "####### RESTFUL-BOOKER-PLATFORM #######
+####                               ####
+####       PRE FLIGHT CHECKS       ####
+####                               ####
+#######################################\n"
+
+
+if ! type -p java; then
+    printf "The Java Runtime Environment is missing. To learn how to install it please visit:
+    https://automationintesting.com/setup/settingupjava\n"
+    exit
+fi
+
+if ! type -p javac; then
+    printf "The Java Development Kit is missing. To learn how to install it please visit:
+    https://automationintesting.com/setup/settingupjava\n"
+    exit
+fi
+
+if ! type -p mvn; then
+    printf "Maven is missing. To learn how to install it please visit:
+    https://automationintesting.com/setup/settingupmaven\n"
+    exit
+fi
+
+if [[ -z "${JAVA_HOME}" ]]; then
+  printf "JAVA_HOME has not been set. To learn how to set it please visit:
+    https://automationintesting.com/setup/settingupmaven\n"
+  exit
+fi
+
+if ! node -v; then
+    printf "Node is missing or broken. To learn how to install it please visit:
+    https://automationintesting.com/setup/settingupnode\n"
+    exit
+fi
+
+if ! type -p npm; then
+    printf "Npm is missing. To learn how to install it please visit:
+    https://automationintesting.com/setup/settingupnode\n"
+    exit
+fi
+
+mvn clean
+
+printf "\n####### RESTFUL-BOOKER-PLATFORM #######
+####                               ####
+####      BUILDING FRONTEND        ####
+####                               ####
+#######################################\n"
+
+cd ui/js
+npm install
+npm run test
+npm run build
+
+printf "\n####### RESTFUL-BOOKER-PLATFORM #######
+####                               ####
+####       BUILDING BACKEND        ####
+####                               ####
+#######################################\n"
+
+cd ../..
+
+
+if [[ -z "${APPLITOOLS_API_KEY}" ]]; then
+  printf "Skipping visual checks because no applitools api key has been set. Assign a key to APPLITOOLS_API_KEY to run visual checks"
+  mvn install -Dvisual.skip.test=true
+else
+  mvn install
+fi
+
+/bin/bash ./run_locally.sh
--- run_locally.sh
+++ run_locally.sh
@@ -1,77 +1,12 @@
 #!/bin/sh
 
-printf "####### RESTFUL-BOOKER-PLATFORM #######
-####                               ####
-####       PRE FLIGHT CHECKS       ####
-####                               ####
-#######################################\n"
-
-
-if ! type -p java; then
-    printf "The Java Runtime Environment is missing. To learn how to install it please visit:
-    https://automationintesting.com/setup/settingupjava\n"
-    exit
-fi
-
-if ! type -p javac; then
-    printf "The Java Development Kit is missing. To learn how to install it please visit:
-    https://automationintesting.com/setup/settingupjava\n"
-    exit
-fi
-
-if ! type -p mvn; then
-    printf "Maven is missing. To learn how to install it please visit:
-    https://automationintesting.com/setup/settingupmaven\n"
-    exit
-fi
-
-if [[ -z "${JAVA_HOME}" ]]; then
-  printf "JAVA_HOME has not been set. To learn how to set it please visit:
-    https://automationintesting.com/setup/settingupmaven\n"
-  exit
-fi
-
-if ! node -v; then
-    printf "Node is missing or broken. To learn how to install it please visit:
-    https://automationintesting.com/setup/settingupnode\n"
-    exit
-fi
-
-if ! type -p npm; then
-    printf "Npm is missing. To learn how to install it please visit:
-    https://automationintesting.com/setup/settingupnode\n"
-    exit
-fi
-
-mvn clean
-
-printf "\n####### RESTFUL-BOOKER-PLATFORM #######
-####                               ####
-####      BUILDING FRONTEND        ####
-####                               ####
-#######################################\n"
-
-cd ui/js
-npm install
-npm run test
-npm run build
-
-printf "\n####### RESTFUL-BOOKER-PLATFORM #######
-####                               ####
-####       BUILDING BACKEND        ####
-####                               ####
-#######################################\n"
-
-cd ../..
-
-
-if [[ -z "${APPLITOOLS_API_KEY}" ]]; then
-  printf "Skipping visual checks because no applitools api key has been set. Assign a key to APPLITOOLS_API_KEY to run visual checks"
-  mvn install -Dvisual.skip.test=true
-else
-  mvn install
-fi
-
+while getopts e: option
+  do
+    case "${option}"
+  in
+    e) DO_E2E=${OPTARG};;
+  esac
+done
 
 printf "\n####### RESTFUL-BOOKER-PLATFORM #######
 ####                               ####
@@ -96,9 +31,14 @@ printf "\n####### RESTFUL-BOOKER-PLATFORM #######
 ####                               ####
 #######################################\n"
 
-cd end-to-end-tests
+if [ "$DO_E2E" = "true" ]; then
+  cd end-to-end-tests
 
-mvn clean test
+  mvn clean test
+else
+printf "\n          SKIPPING E2E TESTS
+ Add -e true argument to run E2E tests\n"
+fi
 
 printf "\n####### RESTFUL-BOOKER-PLATFORM #######
 ####                               ####
