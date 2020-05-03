66527bbd680a62dc5ac34be4bd76ca23b9283a1e
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2020-05-03 14:30:42 +0100

    Fixed broken test and dependency warnings Changes to webpack broke the assets API approval test and was getting a weird warning around moment from JS

--- assets/js/tests/report-test.js
+++ assets/js/tests/report-test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Report from '../src/js/components/Report.jsx';
-import moment from 'moment';
+import moment from 'moment/moment';
 import nock from 'nock';
 
 test('Multiple reports can be created in the Report component', (done) => {
