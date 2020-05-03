9d19dbcbd00312927b3ce69a90f27ef4440bc440
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2020-05-03 21:46:01 +0100

    Bumped dependencies for Report Needed to bump depdencies and fix Wiremock conflict with Spring-boot

--- report/src/test/java/com/automationintesting/integration/BuildReportIT.java
+++ report/src/test/java/com/automationintesting/integration/BuildReportIT.java
@@ -1,57 +1,53 @@
 package com.automationintesting.integration;
 
 import com.automationintesting.api.ReportApplication;
-import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.xebialabs.restito.server.StubServer;
 import io.restassured.response.Response;
 import org.approvaltests.Approvals;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.xebialabs.restito.builder.stub.StubHttp.whenHttp;
+import static com.xebialabs.restito.semantics.Action.ok;
+import static com.xebialabs.restito.semantics.Action.stringContent;
+import static com.xebialabs.restito.semantics.Action.header;
+import static com.xebialabs.restito.semantics.Condition.get;
+import static com.xebialabs.restito.semantics.Condition.parameter;
 import static io.restassured.RestAssured.given;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = ReportApplication.class)
 @ActiveProfiles("dev")
 public class BuildReportIT {
 
-    @Rule
-    public WireMockRule roomApi = new WireMockRule(WireMockConfiguration.options().notifier(new ConsoleNotifier(true)).port(3001));
-
-    @Rule
-    public WireMockRule bookingApi = new WireMockRule(3000);
+    private StubServer roomApi = new StubServer(3001).run();
+    private StubServer bookingApi = new StubServer(3000).run();
 
     @Before
-    public void setupStubs(){
-        roomApi.stubFor(get("/room")
-                .willReturn(
-                    aResponse()
-                        .withHeader("Content-Type","application/json")
-                        .withBody("{\"rooms\":[{\"roomid\":1,\"roomNumber\":101,\"type\":\"Twin\",\"beds\":2,\"accessible\":false,\"details\":\"Wifi, TV, Mini-bar\"}, {\"roomid\":2,\"roomNumber\":102,\"type\":\"Single\",\"beds\":3,\"accessible\":true,\"details\":\"Wifi, TV, Mini-bar\"}]}")
-                        .withStatus(200)));
+    public void setupRestito(){
+        whenHttp(roomApi).
+                match(get("/room")).
+                then(ok(), header("Content-Type","application/json"), stringContent("{\"rooms\":[{\"roomid\":1,\"roomNumber\":101,\"type\":\"Twin\",\"beds\":2,\"accessible\":false,\"details\":\"Wifi, TV, Mini-bar\"}, {\"roomid\":2,\"roomNumber\":102,\"type\":\"Single\",\"beds\":3,\"accessible\":true,\"details\":\"Wifi, TV, Mini-bar\"}]}"));
 
-        bookingApi.stubFor(get("/booking/?roomid=1")
-                .willReturn(
-                    aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("{\"bookings\":[{\"bookingid\":1,\"roomid\":1,\"firstname\":\"James\",\"lastname\":\"Dean\",\"totalprice\":100,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-01-01\",\"checkout\":\"2018-01-05\"}},{\"bookingid\":2,\"roomid\":1,\"firstname\":\"Mark\",\"lastname\":\"Winteringham\",\"totalprice\":200,\"depositpaid\":false,\"bookingdates\":{\"checkin\":\"2018-02-01\",\"checkout\":\"2018-02-05\"}}]}")
-                        .withStatus(200)));
+        whenHttp(bookingApi).
+                match(get("/booking/"), parameter("roomid", "1")).
+                then(ok(),  header("Content-Type","application/json"), stringContent("{\"bookings\":[{\"bookingid\":1,\"roomid\":1,\"firstname\":\"James\",\"lastname\":\"Dean\",\"totalprice\":100,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-01-01\",\"checkout\":\"2018-01-05\"}},{\"bookingid\":2,\"roomid\":1,\"firstname\":\"Mark\",\"lastname\":\"Winteringham\",\"totalprice\":200,\"depositpaid\":false,\"bookingdates\":{\"checkin\":\"2018-02-01\",\"checkout\":\"2018-02-05\"}}]}"));
 
-        bookingApi.stubFor(get("/booking/?roomid=2")
-                .willReturn(
-                    aResponse()
-                            .withHeader("Content-Type", "application/json")
-                            .withBody("{\"bookings\":[{\"bookingid\":1,\"roomid\":2,\"firstname\":\"James\",\"lastname\":\"Dean\",\"totalprice\":100,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-03-01\",\"checkout\":\"2018-03-05\"}},{\"bookingid\":2,\"roomid\":2,\"firstname\":\"Mark\",\"lastname\":\"Winteringham\",\"totalprice\":200,\"depositpaid\":false,\"bookingdates\":{\"checkin\":\"2018-04-01\",\"checkout\":\"2018-04-05\"}}]}")
-                            .withStatus(200)));
+        whenHttp(bookingApi).
+                match(get("/booking/"), parameter("roomid", "2")).
+                then(ok(),  header("Content-Type","application/json"), stringContent("{\"bookings\":[{\"bookingid\":1,\"roomid\":2,\"firstname\":\"James\",\"lastname\":\"Dean\",\"totalprice\":100,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-03-01\",\"checkout\":\"2018-03-05\"}},{\"bookingid\":2,\"roomid\":2,\"firstname\":\"Mark\",\"lastname\":\"Winteringham\",\"totalprice\":200,\"depositpaid\":false,\"bookingdates\":{\"checkin\":\"2018-04-01\",\"checkout\":\"2018-04-05\"}}]}"));
+    }
 
+    @After
+    public void stopServer(){
+        roomApi.stop();
+        bookingApi.stop();
     }
 
     @Test
