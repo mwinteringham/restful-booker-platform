e3eaba2dde337b29f89a18b5d3394f45774ee229
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2022-03-01 20:52:18 +0000

    Bumped API dependencies

--- message/src/main/java/com/automationintesting/api/MessageApplication.java
+++ message/src/main/java/com/automationintesting/api/MessageApplication.java
@@ -3,11 +3,9 @@ package com.automationintesting.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.automationintesting")
-@EnableOpenApi
 public class MessageApplication {
 
     public static void main(String[] args) {
