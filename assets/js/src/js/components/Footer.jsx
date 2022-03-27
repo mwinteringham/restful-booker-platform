f98b76db8bbceda4a1ec6396000948875e216765
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2022-03-27 14:25:12 +0100

    Bumped API versions to 1.4

--- assets/js/src/js/components/Footer.jsx
+++ assets/js/src/js/components/Footer.jsx
@@ -10,7 +10,7 @@ export default class Footer extends React.Component {
       return(
             <footer id="footer" className="footer">
                 <div className="container">
-                    <p className="text-muted">restful-booker-platform v1.3 - Created by <a href="http://www.mwtestconsultancy.co.uk">Mark Winteringham</a> / <a href="https://thefriendlytester.co.uk/">Richard Bradshaw</a> - &copy; 2019-20 <a href={'/#/cookie'}>Cookie-Policy</a> - <a href={'/#/privacy'}>Privacy-Policy</a> - <a href={'/#/admin'}>Admin panel</a> <span style={{float : "right"}}>Learn more about <a href="https://automationintesting.com" alt="Automation in testing site">Automation in Testing</a> here</span></p>
+                    <p className="text-muted">restful-booker-platform v1.4 - Created by <a href="http://www.mwtestconsultancy.co.uk">Mark Winteringham</a> / <a href="https://thefriendlytester.co.uk/">Richard Bradshaw</a> - &copy; 2019-20 <a href={'/#/cookie'}>Cookie-Policy</a> - <a href={'/#/privacy'}>Privacy-Policy</a> - <a href={'/#/admin'}>Admin panel</a> <span style={{float : "right"}}>Learn more about <a href="https://automationintesting.com" alt="Automation in testing site">Automation in Testing</a> here</span></p>
                 </div>
             </footer>);
     }
