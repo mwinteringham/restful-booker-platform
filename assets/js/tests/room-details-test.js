4e43c099e34c6ada296b16842899e8db99516f66
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2022-05-10 17:29:56 +0100

    Moved room details test to RTL

--- assets/js/tests/room-details-test.js
+++ assets/js/tests/room-details-test.js
@@ -2,152 +2,139 @@ import React from 'react';
 import RoomDetails from '../src/js/components/RoomDetails.jsx';
 import nock from 'nock';
 
+import {
+    render, waitFor, fireEvent
+  } from '@testing-library/react'
+
+const roomObject = {
+    roomid: 1,
+    roomName: "101",
+    type: "Single",
+    accessible: true,
+    image: "https://www.mwtestconsultancy.co.uk/img/testim/room2.jpg",
+    description: "Aenean porttitor mauris sit amet lacinia molestie. In posuere accumsan aliquet. Maecenas sit amet nisl massa. Interdum et malesuada fames ac ante.",
+    features: [
+        "TV, WiFi, Safe"
+    ],
+    roomPrice: 100
+}
+
 nock('http://localhost')
     .persist()
-    .get('/room/1')
-    .reply(200, {
-        roomid: 1,
-        roomName: "101",
-        type: "Single",
-        accessible: true,
-        image: "https://www.mwtestconsultancy.co.uk/img/testim/room2.jpg",
-        description: "Aenean porttitor mauris sit amet lacinia molestie. In posuere accumsan aliquet. Maecenas sit amet nisl massa. Interdum et malesuada fames ac ante.",
-        features: [
-            "TV, WiFi, Safe"
-        ],
-        roomPrice: 100
-    });
+    .get('/booking/?roomid=1')
+    .reply(200, {})
 
-test('Room details component renders', () => {
-    const roomDetailsComponent = shallow(
+test('Room details component renders', async () => {
+    const roomMock = nock('http://localhost')
+                        .get('/room/1')
+                        .reply(200, roomObject);
+
+    const {asFragment, getByText} = render(
         <RoomDetails params={{id : 1}} />
     )
 
-    expect(roomDetailsComponent).toMatchSnapshot();
+    await waitFor(() => {expect(roomMock.isDone()).toBeTruthy()})
+    
+    expect(asFragment()).toMatchSnapshot();
 });
 
-test('Room details switches into edit mode', () => {
-    const roomDetailsComponent = shallow(
+test('Room details switches into edit mode', async () => {
+    const roomMock = nock('http://localhost')
+                        .get('/room/1')
+                        .reply(200, roomObject);
+
+    const {asFragment, getByText} = render(
         <RoomDetails params={{id : 1}} />
     )
 
-    roomDetailsComponent.find('.room-details button').simulate('click');
+    await waitFor(() => {expect(roomMock.isDone()).toBeTruthy()})
+    
+    fireEvent.click(getByText(/Edit/));
 
-    expect(roomDetailsComponent).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
 });
 
-test('Room details can be switched out of edit mode', () => {
-    const roomDetailsComponent = shallow(
+test('Room details can be switched out of edit mode', async () => {
+    const roomMock = nock('http://localhost')
+                        .get('/room/1')
+                        .reply(200, roomObject);
+
+    const {asFragment, getByText} = render(
         <RoomDetails params={{id : 1}} />
     )
 
-    roomDetailsComponent.find('.room-details button').simulate('click');
-    roomDetailsComponent.find('#cancelEdit').simulate('click');
+    await waitFor(() => {expect(roomMock.isDone()).toBeTruthy()})
+    
+    fireEvent.click(getByText(/Edit/));
+    fireEvent.click(getByText(/Cancel/));
 
-    expect(roomDetailsComponent).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
 });
 
-test('Room details can render validation errors', () => {
-    nock('http://localhost')
-        .put('/room/1')
-        .reply(400, {
-            "timestamp": "2020-05-02T21:54:40.530+0000",
-            "status": 400,
-            "error": "Bad Request",
-            "errors": [{
-                "codes": [
-                    "Pattern.room.type",
-                    "Pattern.type",
-                    "Pattern.java.lang.String",
-                    "Pattern"
-                ],
-                "arguments": [{
-                        "codes": [
-                            "room.type",
-                            "type"
-                        ],
-                        "defaultMessage": "type",
-                        "code": "type"
-                    },
-                    [],
-                    {
-                        "defaultMessage": "Single|Double|Twin|Family|Suite",
-                        "codes": [
-                            "Single|Double|Twin|Family|Suite"
-                        ]
-                    }
-                ],
-                "defaultMessage": "Type can only contain the room options Single, Double, Twin, Family or Suite",
-                "objectName": "room",
-                "field": "type",
-                "rejectedValue": "string",
-                "bindingFailure": false,
-                "code": "Pattern"
-            }],
-            "message": "Validation failed for object='room'. Error count: 1",
-            "path": "/room/1"
-        });
+test('Room details can render validation errors', async () => {
+    const roomMock = nock('http://localhost')
+                        .get('/room/1')
+                        .reply(200, roomObject);
 
-    const roomDetailsComponent = shallow(
+    const putMock = nock('http://localhost')
+                        .put('/room/1')
+                        .reply(400, {
+                            "errorCode": 400,
+                            "error": "BAD_REQUEST",
+                            "errorMessage": "Validation failed for argument [0] in public org.springframework.http.ResponseEntity<com.automationintesting.model.db.Room> com.automationintesting.api.RoomController.updateRoom(com.automationintesting.model.db.Room,int,java.lang.String) throws java.sql.SQLException: [Field error in object 'room' on field 'type': rejected value [single]; codes [Pattern.room.type,Pattern.type,Pattern.java.lang.String,Pattern]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [room.type,type]; arguments []; default message [type],[Ljavax.validation.constraints.Pattern$Flag;@21a98ac5,Single|Double|Twin|Family|Suite]; default message [Type can only contain the room options Single, Double, Twin, Family or Suite]] ",
+                            "fieldErrors": ["Type can only contain the room options Single, Double, Twin, Family or Suite"]
+                        });
+
+    const {asFragment, getByText} = render(
         <RoomDetails params={{id : 1}} />
     )
 
-    roomDetailsComponent.find('.room-details button').simulate('click');
-    roomDetailsComponent.find('#update').simulate('click');
+    await waitFor(() => {expect(roomMock.isDone()).toBeTruthy()})
 
-    expect(roomDetailsComponent).toMatchSnapshot();
+    fireEvent.click(getByText(/Edit/));
+    fireEvent.click(getByText(/Update/));
+
+    await waitFor(() => {expect(putMock.isDone()).toBeTruthy()})
+
+    expect(asFragment()).toMatchSnapshot();
 });
 
-test('Room details can be submitted', () => {
+test('Room details can be submitted', async () => {
+    const roomMock = nock('http://localhost')
+                        .persist()
+                        .get('/room/1')
+                        .reply(200, roomObject);
+
     let putMock = nock('http://localhost')
-                    .put('/room/1',  {
-                        roomId : 1,
-                        roomName : "999",
-                        accessible : true,
-                        image	: 'test/url',
-                        description : 'Test description',
-                        featuresObject : {
-                            WiFi : false,
-                            TV : false,
-                            Radio : true,
-                            Refreshments : true,
-                            Safe : false,
-                            Views : true
-                        },
-                        type: "Single",
-                        features: ["Radio", "Refreshments", "Views"]
+                    .put('/room/1', {
+                        "roomid": 1,
+                        "roomName": "101",
+                        "type": "Single",
+                        "accessible": true,
+                        "image": "https://www.mwtestconsultancy.co.uk/img/testim/room2.jpg",
+                        "description": "Aenean porttitor mauris sit amet lacinia molestie. In posuere accumsan aliquet. Maecenas sit amet nisl massa. Interdum et malesuada fames ac ante.",
+                        "features": ["TV, WiFi, Safe"],
+                        "roomPrice": 100,
+                        "featuresObject": {
+                            "WiFi": false,
+                            "TV": false,
+                            "Radio": false,
+                            "Refreshments": false,
+                            "Safe": false,
+                            "Views": false,
+                            "TV, WiFi, Safe": true
+                        }
                     })
-                    .reply(202, {});
+                    .reply(202);
     
-    const roomDetailsComponent = shallow(
+    const {getByText} = render(
         <RoomDetails params={{id : 1}} />
     )
-    
-    roomDetailsComponent.setState({
-        room : {
-            roomId : 1,
-            roomName : "999",
-            accessible : true,
-            image	: 'test/url',
-            description : 'Test description',
-            featuresObject : {
-                WiFi : false,
-                TV : false,
-                Radio : true,
-                Refreshments : true,
-                Safe : false,
-                Views : true
-            },
-            type : 'Single',
-            features : []
-        }
-    });
 
-    roomDetailsComponent.update();
-    roomDetailsComponent.instance().doEdit();
+    await waitFor(() => {expect(roomMock.isDone()).toBeTruthy()})
 
-    setTimeout(() => {
-        let didNockAcceptRequest = putMock.isDone();
-        expect(didNockAcceptRequest).toBe(true);
-    }, 0);
+    fireEvent.click(getByText(/Edit/));
+    fireEvent.click(getByText(/Update/));
+
+    await waitFor(() => {expect(putMock.isDone()).toBeTruthy()})
 });
