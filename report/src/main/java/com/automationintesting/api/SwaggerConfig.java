c3a7add541e8dd02503b4b4b8f05ec5c5aed554a
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2022-06-07 21:20:20 +0100

    Fixed deploy script and testing out new Swagger server option

--- report/src/main/java/com/automationintesting/api/SwaggerConfig.java
+++ report/src/main/java/com/automationintesting/api/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package com.automationintesting.api;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/report/"));
+    }
 
     @Bean
     public GroupedOpenApi publicApi() {
