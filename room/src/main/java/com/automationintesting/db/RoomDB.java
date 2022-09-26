65be1b03d067319ca2db7575832ac6bc43e5351b
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2022-09-26 11:41:31 +0100

    Fixed duplicate port number [ci skip]

--- room/src/main/java/com/automationintesting/db/RoomDB.java
+++ room/src/main/java/com/automationintesting/db/RoomDB.java
@@ -43,7 +43,7 @@ public class RoomDB {
         // the environmental variable dbServer to true.
         try{
             if(System.getenv("dbServer").equals("true")){
-                Server.createTcpServer("-tcpPort", "9091", "-tcpAllowOthers").start();
+                Server.createTcpServer("-tcpPort", "9094", "-tcpAllowOthers").start();
                 logger.info("DB server mode enabled");
             } else {
                 logger.info("DB server mode disabled");
